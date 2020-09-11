033c940b337cb0491077dbdbcc18f70eb6b41b81
Jesse Klaasse <jesse.klaasse@indicia.nl>
2020-09-11 10:19:46 +0200

    chore: add comments

--- Indicia.HubSpot/Core/IHubSpotObject.cs
+++ Indicia.HubSpot/Core/IHubSpotObject.cs
@@ -7,6 +7,9 @@
         /// </summary>
         string ObjectType { get; }
         
+        /// <summary>
+        /// The HubSpot object identifier
+        /// </summary>
         string Id { get; set; }
     }
 }
